dcea3c68f2143cfe4f106b157f11e160b3d845be
Xavier Tang <xavier.tang109@gmail.com>
2025-03-17 01:22:11 +1030

    begin refactoring

--- main.js
+++ main.js
@@ -28,92 +28,108 @@ const months = [
   "Dec",
 ];
 let results = [];
-//parse the csv file first
-fs.createReadStream("./calendar.csv")
-  .pipe(csv())
-  .on("data", (data) => results.push(data))
-  .on("end", () => {
-    for (let i = 0; i < results.length; i++) {
-      let start;
-      let end;
-      let duration;
-      try {
-        let date = results[i].DATE.split(" - ");
-        start = date[0].split(" ");
-        end = date[1].split(" ");
-        duration =
-          Math.ceil(
-            (new Date(year, months.indexOf(end[1]), end[0]) -
-              new Date(year, months.indexOf(start[1]), start[0])) /
-              (24 * 60 * 60 * 7 * 1000),
-          ) + 1;
-      } catch {
-        let date = results[i].DATE;
-        start = date.split(" ");
-        duration = 1;
-      }
 
-      let startTime;
-      let endTime;
-      let hours;
-      let minutes;
-      try {
-        let time = results[i].DURATION.split(" - ");
-        startTime = time[0].split(" ");
-        endTime = time[1].split(" ");
-        startTime[0] = startTime[0].split(":");
-        startTime[0][0] = parseInt(startTime[0][0]);
-        startTime[0][1] = parseInt(startTime[0][1]);
-        endTime[0] = endTime[0].split(":");
-        endTime[0][0] = parseInt(endTime[0][0]);
-        endTime[0][1] = parseInt(endTime[0][1]);
-        if (startTime[1] == "PM" && startTime[0][0] != 12) {
-          startTime[0][0] += 12;
-        }
-        if (endTime[1] == "PM" && endTime[0][0] != 12) {
-          endTime[0][0] += 12;
-        }
-      } catch {
-        console.log("Error occured in parsing time");
-        console.log(`Please check line ${i + 2} for more details.`);
-      }
-      let currTime = new Date(
+function parseItem(item, subject_name, item_name) {
+  events = [];
+  console.log(item);
+  let start;
+  let end;
+  let duration;
+  try {
+    let date = item[0].split(" - ");
+    start = date[0].split(" ");
+    end = date[1].split(" ");
+    duration =
+      Math.ceil(
+        (new Date(year, months.indexOf(end[1]), end[0]) -
+          new Date(year, months.indexOf(start[1]), start[0])) /
+          (24 * 60 * 60 * 7 * 1000),
+      ) + 1;
+  } catch {
+    console.error("CSV not formatted properly. Check all dates and time");
+    return;
+  }
+
+  let startTime;
+  let endTime;
+  let hours;
+  let minutes;
+  try {
+    let time = item[2].split(" - ");
+    startTime = time[0].split(" ");
+    endTime = time[1].split(" ");
+    startTime[0] = startTime[0].split(":");
+    startTime[0][0] = parseInt(startTime[0][0]);
+    startTime[0][1] = parseInt(startTime[0][1]);
+    endTime[0] = endTime[0].split(":");
+    endTime[0][0] = parseInt(endTime[0][0]);
+    endTime[0][1] = parseInt(endTime[0][1]);
+    if (startTime[1] == "PM" && startTime[0][0] != 12) {
+      startTime[0][0] += 12;
+    }
+    if (endTime[1] == "PM" && endTime[0][0] != 12) {
+      endTime[0][0] += 12;
+    }
+  } catch {
+    console.log("Error occured in parsing time");
+    console.log(`Please check line ${i + 2} for more details.`);
+  }
+  let currTime = new Date(
+    year,
+    months.indexOf(start[1]),
+    start[0],
+    startTime[0][0],
+    startTime[0][1],
+  );
+  for (let j = 0; j < duration; j++) {
+    let event = {
+      start: [
         year,
-        months.indexOf(start[1]),
-        start[0],
+        currTime.getMonth() + 1,
+        currTime.getDate(),
         startTime[0][0],
         startTime[0][1],
-      );
-      for (let j = 0; j < duration; j++) {
-        let event = {
-          start: [
-            year,
-            currTime.getMonth() + 1,
-            currTime.getDate(),
-            startTime[0][0],
-            startTime[0][1],
-          ],
-          end: [
-            year,
-            currTime.getMonth() + 1,
-            currTime.getDate(),
-            endTime[0][0],
-            endTime[0][1],
-          ],
-          title: results[i].TITLE,
-          description: results[i].DESCRIPTION,
-          location: results[i].LOCATION,
-        };
-        currTime = new Date(currTime.getTime() + 7 * 24 * 60 * 60 * 1000);
+      ],
+      end: [
+        year,
+        currTime.getMonth() + 1,
+        currTime.getDate(),
+        endTime[0][0],
+        endTime[0][1],
+      ],
+      title: item_name,
+      description: subject_name,
+      location: item[3],
+    };
+    currTime = new Date(currTime.getTime() + 7 * 24 * 60 * 60 * 1000);
 
-        events.push(event);
-      }
+    events.push(event);
+  }
+}
+return events;
+title = "";
+subject = "";
+//parse the csv file first
+text = fs.readFileSync("calendar.csv", "utf8");
+
+results = text.split("\n");
+for (let i = 0; i < results.length; i++) {
+  line = results[i];
+  split_line = line.split(",");
+  if (split_line[1] == "") {
+    if (line.search("-") == -1) {
+      title = split_line[0];
+    } else {
+      subject = split_line[0];
     }
-    ics.createEvents(events, (error, value) => {
-      if (error) {
-        console.log(error);
-        return;
-      }
-      fs.writeFileSync("calendar.ics", value);
-    });
-  });
+  } else {
+    events = events.concat(parseItem(split_line, subject, title));
+  }
+}
+ics.createEvents(events, (error, value) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  fs.writeFileSync("calendar.ics", value);
+});
